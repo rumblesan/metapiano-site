de36fb18eb482fa49cfa834bcbb45e73f392a2e8
Guy John <guy@rumblesan.com>
2013-07-27 11:41:00 +0100

    Updating DB startup code and config

--- app/Global.scala
+++ app/Global.scala
@@ -8,35 +8,35 @@ import play.api.Application
 
 object Global extends GlobalSettings {
 
-  /** This implements basic DB session management
-   *
-   *  If the application mode is test then it will use the H2 in memory DB
-   *  Otherwise it will use the specified database for that mode
-   */
-
   override def onStart(app: Application) {
-    SessionFactory.concreteFactory = {
 
-      app.configuration.getString("application.mode") match {
-        case Some("test") => {
-          case Some("org.h2.Driver") => Some(() => getSession(new H2Adapter, app))
-        }
-        case Some(mode) => {
-          "db.%s.driver".format(mode) match {
-            // Uncomment these as needed
-            // case Some("org.h2.Driver") => Some(() => getSession(new H2Adapter, app))
-            // case Some("org.postgresql.Driver") => Some(() => getSession(new PostgreSqlAdapter, app))
-            case _ => sys.error("Database driver must be either org.h2.Driver or org.postgresql.Driver")
-          }
+    val conf = app.configuration
 
-        }
-        case None => sys.error("Need to specify application mode")
+    val dbName = conf.getString("application.database").getOrElse("default")
+
+    SessionFactory.concreteFactory = conf.getConfig("db." + dbName).flatMap(
+      _.getString("driver")
+    ) match {
+      case Some("org.h2.Driver") => {
+        Some(
+          () => getSession(new H2Adapter, dbName, app)
+        )
       }
-
+      case Some("org.postgresql.Driver") => {
+        Some(
+          () => getSession(new PostgreSqlAdapter, dbName, app)
+        )
+      }
+      case _ => sys.error("Database driver must be either org.h2.Driver or org.postgresql.Driver")
     }
+
   }
 
-  def getSession(adapter:DatabaseAdapter, app: Application) = Session.create(DB.getConnection()(app), adapter)
+  def getSession(adapter:DatabaseAdapter, dbName: String, app: Application) = {
+    Session.create(
+      DB.getConnection(dbName)(app),
+      adapter
+    )
+  }
 
 }
-
