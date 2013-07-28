975ad77df3e29df29212d3406927286d363e48c9
Guy John <guy@rumblesan.com>
2013-07-28 22:34:01 +0100

    Adding poem submission and viewing functionality

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,17 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import models.Poem
+
+import play.api.data._
+import play.api.data.Forms._
+
+
 object Application extends Controller {
+
+  val poemForm = Form(
+    "text" -> text
+  )
 
   def about = Action {
     Ok(views.html.about())
@@ -13,12 +23,29 @@ object Application extends Controller {
     Ok(views.html.write())
   }
 
-  def submit = Action {
-    Ok(views.html.submit())
+  def submit = Action { implicit request =>
+    poemForm.bindFromRequest.fold(
+      formWithErrors =>
+        Ok(formWithErrors.toString),
+      value => {
+
+        Poem.create(
+          Poem(
+            0,
+            value
+          )
+        )
+
+        Ok(views.html.submit())
+      })
   }
 
   def poems = Action {
-    Ok(views.html.poems())
+    Ok(
+      views.html.poems(
+        Poem.retrieveAll()
+      )
+    )
   }
 
   def stream = Action {
